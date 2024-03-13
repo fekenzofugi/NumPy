53a08bb068074d969ccd92facdcc1c32b8018720
fekenzofugi <fernandofugihara@gmail.com>
2024-03-12 21:00:42 -0300

    operators

---
+++ programs/operators.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+arr = np.arange(1, 10)
+
+# sum of arrays
+print("The sum of arrays is {}".format(arr + arr))
+
+# subtraction of arrays
+print("The subtraction of arrays is {}".format(arr - arr))
+
+# product of arrays
+print("The product of arrays is {}".format(arr * arr))
+
+# division of arrays
+print("The division of arrays is {}".format(arr / arr))
+
+# power
+print("The elements of the arrays power by 2 are {}".format(arr ** 2))
+
+# square root
+print("The square root of the elements are {}".format(np.sqrt(arr)))
